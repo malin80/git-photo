3d2028d68c9d6820c9c16f269eb499f2095be7be
malin <malin@malindeMacBook-Pro.local>
2017-07-24 07:41:20 +0800

    Initial Commit

---
+++ Photo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photo
+//
+//  Created by malin  on 2017/7/24.
+//  Copyright © 2017年 malin . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
