f213b9654f55a6714815ea442ed5bc0812a9e55c
malin <@450749998@qq.com>
2017-08-23 10:01:49 +0800

    首页按钮

--- Photo/HomePage/ViewController/View/HomePageButtonView.h
+++ Photo/HomePage/ViewController/View/HomePageButtonView.h
@@ -22,11 +22,16 @@
 
 @property (nonatomic, strong) UIButton *cameraButton;
 @property (nonatomic, strong) UIButton *weddingButton;
+@property (nonatomic, strong) UILabel *weddingLabel;
 @property (nonatomic, strong) UIButton *carButton;
 @property (nonatomic, strong) UIButton *flowerButton;
+@property (nonatomic, strong) UILabel *flowerLabel;
 @property (nonatomic, strong) UIButton *hotelButton;
+@property (nonatomic, strong) UILabel *hotelLabel;
 @property (nonatomic, strong) UIButton *videoButton;
+@property (nonatomic, strong) UILabel *videoLabel;
 @property (nonatomic, strong) UIButton *clothButton;
+@property (nonatomic, strong) UILabel *clothLabel;
 @property (nonatomic, strong) UIButton *moreButton;
 
 @property(nonatomic,assign) id<HomePageButtonViewDelegate> delegate;
--- Photo/HomePage/ViewController/View/HomePageButtonView.m
+++ Photo/HomePage/ViewController/View/HomePageButtonView.m
@@ -28,11 +28,16 @@
 - (void)initView {
     [self addSubview:self.cameraButton];
     [self addSubview:self.weddingButton];
+    [self addSubview:self.weddingLabel];
     [self addSubview:self.carButton];
     [self addSubview:self.flowerButton];
+    [self addSubview:self.flowerLabel];
     [self addSubview:self.hotelButton];
+    [self addSubview:self.hotelLabel];
     [self addSubview:self.videoButton];
+    [self addSubview:self.videoLabel];
     [self addSubview:self.clothButton];
+    [self addSubview:self.clothLabel];
     [self addSubview:self.moreButton];
 }
 
@@ -51,21 +56,36 @@
         make.height.equalTo(@(kButtonHeitht));
     }];
     
-    [_carButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.right.equalTo(_flowerButton.mas_left).with.offset(-kButtonDistance);
+    [_weddingLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(_weddingButton);
+        make.bottom.equalTo(_weddingButton.mas_bottom);
+    }];
+    
+    [_clothButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_weddingButton.mas_right).with.offset(kButtonDistance);
         make.top.equalTo(_cameraButton);
         make.width.equalTo(@(kButtonWidth));
         make.height.equalTo(@(kButtonHeitht));
     }];
     
-    [_flowerButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.right.equalTo(self.mas_right).with.offset(-10);
-        make.top.equalTo(_cameraButton);
-        make.width.equalTo(@(kButtonWidth));
-        make.height.equalTo(@(kButtonHeitht));
+    [_clothLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(_clothButton);
+        make.bottom.equalTo(_clothButton.mas_bottom);
     }];
-    
+
     [_hotelButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_cameraButton);
+        make.left.equalTo(_clothButton.mas_right).with.offset(kButtonDistance);
+        make.width.equalTo(@(kButtonWidth));
+        make.height.equalTo(@(kButtonHeitht));
+    }];
+    
+    [_hotelLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(_hotelButton);
+        make.bottom.equalTo(_hotelButton.mas_bottom);
+    }];
+
+    [_carButton mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(_cameraButton.mas_bottom).with.offset(20);
         make.left.equalTo(_cameraButton);
         make.width.equalTo(@(kButtonWidth));
@@ -73,22 +93,32 @@
     }];
     
     [_videoButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(_hotelButton);
-        make.left.equalTo(_weddingButton);
+        make.top.equalTo(_carButton);
+        make.left.equalTo(_carButton.mas_right).with.offset(kButtonDistance);
         make.width.equalTo(@(kButtonWidth));
         make.height.equalTo(@(kButtonHeitht));
     }];
     
-    [_clothButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(_hotelButton);
-        make.right.equalTo(_carButton);
+    [_videoLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(_videoButton);
+        make.bottom.equalTo(_videoButton.mas_bottom);
+    }];
+    
+    [_flowerButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_videoButton.mas_right).with.offset(kButtonDistance);
+        make.top.equalTo(_carButton);
         make.width.equalTo(@(kButtonWidth));
         make.height.equalTo(@(kButtonHeitht));
+    }];
+    
+    [_flowerLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(_flowerButton);
+        make.bottom.equalTo(_flowerButton.mas_bottom);
     }];
     
     [_moreButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(_hotelButton);
-        make.right.equalTo(_flowerButton);
+        make.top.equalTo(_carButton);
+        make.left.equalTo(_flowerButton.mas_right).with.offset(kButtonDistance);
         make.width.equalTo(@(kButtonWidth));
         make.height.equalTo(@(kButtonHeitht));
     }];
@@ -154,16 +184,21 @@
     if (!_weddingButton) {
         _weddingButton = [[UIButton alloc] init];
         [_weddingButton addTarget:self action:@selector(touchWediingButton) forControlEvents:UIControlEventTouchUpInside];
-        
-        [_weddingButton setTitle:@"婚庆" forState:UIControlStateNormal];
-        [_weddingButton setTitleEdgeInsets:UIEdgeInsetsMake(45, -45, 0.0, 0.0)];
-        [_weddingButton setTitleColor:kTextColor forState:UIControlStateNormal];
-        _weddingButton.titleLabel.font = [UIFont systemFontOfSize:14];
-        
         [_weddingButton setImage:[UIImage imageNamed:@"homepage_wedding"] forState:UIControlStateNormal];
         [_weddingButton setImageEdgeInsets:UIEdgeInsetsMake(0.0, 0.0, 20, 0.0)];
     }
     return _weddingButton;
+}
+
+- (UILabel *)weddingLabel {
+    if (!_weddingLabel) {
+        _weddingLabel = [[UILabel alloc] init];
+        _weddingLabel.text = @"婚礼策划";
+        _weddingLabel.textColor = kTextColor;
+        _weddingLabel.font = [UIFont systemFontOfSize:14];
+        [_weddingLabel sizeToFit];
+    }
+    return _weddingLabel;
 }
 
 
@@ -176,7 +211,6 @@
         [_carButton setTitleEdgeInsets:UIEdgeInsetsMake(45, -45, 0.0, 0.0)];
         [_carButton setTitleColor:kTextColor forState:UIControlStateNormal];
         _carButton.titleLabel.font = [UIFont systemFontOfSize:14];
-//        _carButton.titleLabel.adjustsFontSizeToFitWidth=YES;
         [_carButton setImage:[UIImage imageNamed:@"homepage_car"] forState:UIControlStateNormal];
         [_carButton setImageEdgeInsets:UIEdgeInsetsMake(0.0, 0.0, 20, 0.0)];
     }
@@ -188,16 +222,21 @@
     if (!_flowerButton) {
         _flowerButton = [[UIButton alloc] init];
         [_flowerButton addTarget:self action:@selector(touchFlowerButton) forControlEvents:UIControlEventTouchUpInside];
-        
-        [_flowerButton setTitle:@"花艺" forState:UIControlStateNormal];
-        [_flowerButton setTitleEdgeInsets:UIEdgeInsetsMake(45, -45, 0.0, 0.0)];
-        [_flowerButton setTitleColor:kTextColor forState:UIControlStateNormal];
-        _flowerButton.titleLabel.font = [UIFont systemFontOfSize:14];
-        
         [_flowerButton setImage:[UIImage imageNamed:@"homepage_flower"] forState:UIControlStateNormal];
         [_flowerButton setImageEdgeInsets:UIEdgeInsetsMake(0.0, 0.0, 20, 0.0)];
     }
     return _flowerButton;
+}
+
+- (UILabel *)flowerLabel {
+    if (!_flowerLabel) {
+        _flowerLabel = [[UILabel alloc] init];
+        _flowerLabel.text = @"花艺甜品";
+        _flowerLabel.textColor = kTextColor;
+        _flowerLabel.font = [UIFont systemFontOfSize:14];
+        [_flowerLabel sizeToFit];
+    }
+    return _flowerLabel;
 }
 
 
@@ -205,33 +244,42 @@
     if (!_hotelButton) {
         _hotelButton = [[UIButton alloc] init];
         [_hotelButton addTarget:self action:@selector(touchHotelButton) forControlEvents:UIControlEventTouchUpInside];
-        
-        [_hotelButton setTitle:@"酒店" forState:UIControlStateNormal];
-        [_hotelButton setTitleEdgeInsets:UIEdgeInsetsMake(45, -45, 0.0, 0.0)];
-        [_hotelButton setTitleColor:kTextColor forState:UIControlStateNormal];
-        _hotelButton.titleLabel.font = [UIFont systemFontOfSize:14];
-        
         [_hotelButton setImage:[UIImage imageNamed:@"homepage_hotel"] forState:UIControlStateNormal];
         [_hotelButton setImageEdgeInsets:UIEdgeInsetsMake(0.0, 0.0, 20, 0.0)];
     }
     return _hotelButton;
 }
 
+- (UILabel *)hotelLabel {
+    if (!_hotelLabel) {
+        _hotelLabel = [[UILabel alloc] init];
+        _hotelLabel.text = @"婚宴酒店";
+        _hotelLabel.textColor = kTextColor;
+        _hotelLabel.font = [UIFont systemFontOfSize:14];
+        [_hotelLabel sizeToFit];
+    }
+    return _hotelLabel;
+}
 
 - (UIButton *)videoButton {
     if (!_videoButton) {
         _videoButton = [[UIButton alloc] init];
         [_videoButton addTarget:self action:@selector(touchVideoButton) forControlEvents:UIControlEventTouchUpInside];
-        
-        [_videoButton setTitle:@"视频" forState:UIControlStateNormal];
-        [_videoButton setTitleEdgeInsets:UIEdgeInsetsMake(45, -45, 0.0, 0.0)];
-        [_videoButton setTitleColor:kTextColor forState:UIControlStateNormal];
-        _videoButton.titleLabel.font = [UIFont systemFontOfSize:14];
-        
         [_videoButton setImage:[UIImage imageNamed:@"homepage_video"] forState:UIControlStateNormal];
         [_videoButton setImageEdgeInsets:UIEdgeInsetsMake(0.0, 0.0, 20, 0.0)];
     }
     return _videoButton;
+}
+
+- (UILabel *)videoLabel {
+    if (!_videoLabel) {
+        _videoLabel = [[UILabel alloc] init];
+        _videoLabel.text = @"视频广告";
+        _videoLabel.textColor = kTextColor;
+        _videoLabel.font = [UIFont systemFontOfSize:14];
+        [_videoLabel sizeToFit];
+    }
+    return _videoLabel;
 }
 
 
@@ -239,26 +287,29 @@
     if (!_clothButton) {
         _clothButton = [[UIButton alloc] init];
         [_clothButton addTarget:self action:@selector(touchClothButton) forControlEvents:UIControlEventTouchUpInside];
-        
-        [_clothButton setTitle:@"婚纱" forState:UIControlStateNormal];
-        [_clothButton setTitleEdgeInsets:UIEdgeInsetsMake(45, -45, 0.0, 0.0)];
-        [_clothButton setTitleColor:kTextColor forState:UIControlStateNormal];
-        _clothButton.titleLabel.font = [UIFont systemFontOfSize:14];
-        _clothButton.titleLabel.frame = CGRectMake(0, 0, 100, 20);
-        
         [_clothButton setImage:[UIImage imageNamed:@"homepage_cloth"] forState:UIControlStateNormal];
         [_clothButton setImageEdgeInsets:UIEdgeInsetsMake(0.0, 0.0, 20, 0.0)];
     }
     return _clothButton;
 }
 
+- (UILabel *)clothLabel {
+    if (!_clothLabel) {
+        _clothLabel = [[UILabel alloc] init];
+        _clothLabel.text = @"婚纱礼服";
+        _clothLabel.textColor = kTextColor;
+        _clothLabel.font = [UIFont systemFontOfSize:14];
+        [_clothLabel sizeToFit];
+    }
+    return _clothLabel;
+}
 
 - (UIButton *)moreButton {
     if (!_moreButton) {
         _moreButton = [[UIButton alloc] init];
         [_moreButton addTarget:self action:@selector(touchMoreButton) forControlEvents:UIControlEventTouchUpInside];
         
-        [_moreButton setTitle:@"更多" forState:UIControlStateNormal];
+        [_moreButton setTitle:@"司仪" forState:UIControlStateNormal];
         [_moreButton setTitleEdgeInsets:UIEdgeInsetsMake(45, -45, 0.0, 0.0)];
         [_moreButton setTitleColor:kTextColor forState:UIControlStateNormal];
         _moreButton.titleLabel.font = [UIFont systemFontOfSize:14];
